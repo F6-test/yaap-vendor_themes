1f4da38dd3150eef6fdf21258a04c2f9ee39babb
Pranav Vashi <neobuddy89@gmail.com>
2026-06-06 20:57:06 +0530

    fonts: Add ComiCSans
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- fonts.mk
+++ fonts.mk
@@ -14,6 +14,7 @@ PRODUCT_PACKAGES += \
     FontOnePlusSansOverlay \
     FontOneplusSlateSourceOverlay \
     FontRobotoOverlay \
+    FontComicSansOverlay \
     FontRobotoFlexOverlay \
     FontRubikOverlay \
     FontSonySketchOverlay \
